9e1e51d6eea838a974b92859430ea1f3ae23ecfb
VarshitShri <varshit.sf2025@gmail.com>
2025-04-15 12:42:58 +0000

    conversion operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -22,6 +22,40 @@ let booleanLoggedIn = Boolean(isLoggedIn);
 
 let someNumber = 101
 let someString = String(someNumber);
-console.log(typeof someString);
-console.log(someString);
+// console.log(typeof someString);
+// console.log(someString);
+
+// ************************ Operations ************************
+
+let value = 3
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); //Power
+// console.log(4/2);
+// console.log(4%3);
+
+let str1 = "abc"
+let str2 = " xyz"
+let str3 = str1 + str2;
+// console.log(str3);
+
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); //12
+// console.log("1" + "2"); // 12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32
+// console.log(1 + "2" + 2); //122
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
 
