1a00fbb9acb62637d1220e75f9fed937cfee81f5
VarshitShri <varshit.sf2025@gmail.com>
2025-04-15 12:24:22 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,27 @@
+let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 0;
+let booleanLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanLoggedIn);
+ 
+// 1 => true; 0 => false
+// "" => false 
+// "test" => true
+
+let someNumber = 101
+let someString = String(someNumber);
+console.log(typeof someString);
+console.log(someString);
+
