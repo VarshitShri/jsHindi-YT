1cb8c8c67c7837f3e5bc379bc3318f376fac2516
VarshitShri <varshit.sf2025@gmail.com>
2025-04-15 13:04:28 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0); // null converted into number (0) in comparison operators hence false.
+// console.log(null == 0); // equality check operator works differently than comparison operators.
+// console.log(null < 0);
+// console.log(null >= 0);  // returns true bcz 0 is greater or equal to 0
+
+// console.log(undefined < 0);
+// console.log(undefined > 0);
+// console.log(undefined <= 0);
+// console.log(undefined >= 0);
+// console.log(undefined == 0);
+
+// "===" checks for the datatype as well, while comparing #strictCheck
+console.log("2" === 2);
